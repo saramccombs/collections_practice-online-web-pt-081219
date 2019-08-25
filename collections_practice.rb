0da40fcee79e42bd0492880819d310a6062af73d
Sara McCombs <thesaramccombs@gmail.com>
2019-08-25 17:26:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,7 +27,10 @@ end
 # returns an updated array
 
 def swap_elements(array)
-  
+  temp1 = array[1]
+  temp2 = array[2]
+  array[1] = temp1
+  array[2] = temp2
 end
 
 # method named reverse_array
