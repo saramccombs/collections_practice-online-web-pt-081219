6141a45722c68ae72dfb61af9c334357e7d48334
Sara McCombs <thesaramccombs@gmail.com>
2019-08-25 17:00:04 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,3 +4,8 @@ def sort_array_asc(array_of_integers)
   array_of_integers.sort
 end
 
+#method sort_array_desc, takes in an array of integers and returns a copy of the array with the integers in descending order.
+
+def sort_array_desc(array_of_integers)
+  array_of_integers.sort{ |a, b| b <=> a }
+end
