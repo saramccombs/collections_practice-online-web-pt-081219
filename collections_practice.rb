031d424bedae7affa1bb6d66cc9ce294efaa4404
Sara McCombs <thesaramccombs@gmail.com>
2019-08-25 16:46:08 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,5 @@
-# 
+# Build a method sort_array_asc that takes in an array of integers and returns a copy of the array with the integers in ascending order.
+
+def sort_array_asc(array_of_integers)
+  array_of_integers.sort
+end
