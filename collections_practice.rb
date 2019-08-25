4146a628884a2f3771af73ce9ec66a9cf4a447fe
Sara McCombs <thesaramccombs@gmail.com>
2019-08-25 17:02:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,11 +1,23 @@
-# Build a method sort_array_asc that takes in an array of integers and returns a copy of the array with the integers in ascending order.
+# method sort_array_asc
+# takes in an array of integers
+# returns a copy of the array with the integers in ascending order.
 
 def sort_array_asc(array_of_integers)
   array_of_integers.sort
 end
 
-#method sort_array_desc, takes in an array of integers and returns a copy of the array with the integers in descending order.
+# method sort_array_desc
+# takes in an array of integers
+# returns a copy of the array with the integers in descending order.
 
 def sort_array_desc(array_of_integers)
   array_of_integers.sort{ |a, b| b <=> a }
+end
+
+# method sort_array_char_count
+# takes in an array of strings
+# returns a copy of the array with the strings ordered in ascending order by length.
+
+def sort_array_char_count(array_of_strings)
+  array_of_strings.sort {|a, b| a.length <=> b.length}
 end
